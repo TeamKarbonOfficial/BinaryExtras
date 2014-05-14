ebb06ce8ad9a6424ee8c94908d1bdb478ef31499
euwbah <euwbah@gmail.com>
2014-05-14 19:30:13 +0800

    Completed basic functionality of MainActivity. What's the progress bar for and how to use it? XD

--- BinaryExtras/src/com/teamkarbon/android/binaryextras/MainActivity.java
+++ BinaryExtras/src/com/teamkarbon/android/binaryextras/MainActivity.java
@@ -2,6 +2,7 @@ package com.teamkarbon.android.binaryextras;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -28,7 +29,13 @@ public class MainActivity extends Activity {
 	
 	public void Quiz_Click (View view)
 	{
-		
+		Intent intent = new Intent(MainActivity.this, Quiz.class);
+		MainActivity.this.startActivity(intent);
 	}
 
+	public void Converter_Click (View view)
+	{
+		Intent intent = new Intent(MainActivity.this, Converter.class);
+		MainActivity.this.startActivity(intent);
+	}
 }
