9aa4a5bad34c67d897a1a0c3ada39a25e3090113
euwbah <euwbah@gmail.com>
2014-05-16 16:37:35 +0800

    asdfjkl I need help with the thingy

--- BinaryExtras/src/com/teamkarbon/android/binaryextras/LevelSelect.java
+++ BinaryExtras/src/com/teamkarbon/android/binaryextras/LevelSelect.java
@@ -2,8 +2,14 @@ package com.teamkarbon.android.binaryextras;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class LevelSelect extends Activity {
+
+	Spinner spinner;
+	
+	ArrayAdapter<CharSequence> adapter;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -11,6 +17,10 @@ public class LevelSelect extends Activity {
 	    super.onCreate(savedInstanceState);
 	
 	    // TODO Auto-generated method stub
+	    spinner = (Spinner) findViewById(R.id.spinner1);
+	    
+	    //FIXME i need help...
+	    //adapter = ArrayAdapter.createFromResource(this, R.string.noOfQnsSelect, android.R.layout.simple_spinner_item);
 	}
 
 }
